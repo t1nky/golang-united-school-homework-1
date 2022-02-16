e76400af03347b12ce1d3be2ac1dca89e8d9b2c8
t1nky <haiiryy@gmail.com>
2022-02-16 13:39:18 +0500

    Solution

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module homework
 
 go 1.17
+
+require github.com/kyokomi/emoji v2.2.4+incompatible
--- solution.go
+++ solution.go
@@ -1,5 +1,7 @@
 package solution
 
+import "github.com/kyokomi/emoji"
+
 func GetMessage() string {
-	return "Hello :world_map:!"
+	return emoji.Sprint("Hello :world_map:!")
 }
